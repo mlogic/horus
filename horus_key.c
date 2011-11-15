51441776b51587b60b5d3da17b1909f166301b37
Yasuhiro Ohara <yasu@soe.ucsc.edu>
2011-11-15 15:53:24 -0800

    Synchronized with the python code.

--- horus_key.c
+++ horus_key.c
@@ -1,9 +1,12 @@
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /*HAVE_CONFIG_H*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define HAVE_OPENSSL /* XXX */
 #ifdef HAVE_OPENSSL
 #include <openssl/sha.h>
 #include <openssl/hmac.h>
@@ -23,54 +26,87 @@ char *progname;
 #define MIN_CHUNK_SIZE      4096
 
 unsigned int block_size[] = {
-  MIN_CHUNK_SIZE << BRANCH_FACTOR_BITS * 9,
-  MIN_CHUNK_SIZE << BRANCH_FACTOR_BITS * 8,
-  MIN_CHUNK_SIZE << BRANCH_FACTOR_BITS * 7,
-  MIN_CHUNK_SIZE << BRANCH_FACTOR_BITS * 6,
-  MIN_CHUNK_SIZE << BRANCH_FACTOR_BITS * 5,
-  MIN_CHUNK_SIZE << BRANCH_FACTOR_BITS * 4,
-  MIN_CHUNK_SIZE << BRANCH_FACTOR_BITS * 3,
-  MIN_CHUNK_SIZE << BRANCH_FACTOR_BITS * 2,
-  MIN_CHUNK_SIZE << BRANCH_FACTOR_BITS * 1,
-  MIN_CHUNK_SIZE << BRANCH_FACTOR_BITS * 0,
+  MIN_CHUNK_SIZE << (BRANCH_FACTOR_BITS * 9),
+  MIN_CHUNK_SIZE << (BRANCH_FACTOR_BITS * 8),
+  MIN_CHUNK_SIZE << (BRANCH_FACTOR_BITS * 7),
+  MIN_CHUNK_SIZE << (BRANCH_FACTOR_BITS * 6),
+  MIN_CHUNK_SIZE << (BRANCH_FACTOR_BITS * 5),
+  MIN_CHUNK_SIZE << (BRANCH_FACTOR_BITS * 4),
+  MIN_CHUNK_SIZE << (BRANCH_FACTOR_BITS * 3),
+  MIN_CHUNK_SIZE << (BRANCH_FACTOR_BITS * 2),
+  MIN_CHUNK_SIZE << (BRANCH_FACTOR_BITS * 1),
+  MIN_CHUNK_SIZE << (BRANCH_FACTOR_BITS * 0),
 };
+
+#define MIN(x,y) ((x) < (y) ? (x) : (y))
+
+int
+snprint_hex (char *buf, int bufsiz, char *key, int size)
+{
+  int i;
+  for (i = 0; i < MIN ((bufsiz - 1) / 2, size); i++)
+    snprintf (&buf[i * 2], 3, "%02x", (unsigned char) key[i]);
+  return i;
+}
+
+void
+print_hex (char *prefix, char *key, int size)
+{
+  int i;
+  printf ("%s", prefix);
+  for (i = 0; i < size; i++)
+    printf ("%02x", (unsigned char) key[i]);
+  printf (" (size: %d)\n", size);
+}
+
+char *
+print_key (char *key, int key_len)
+{
+  static char buf[SHA_DIGEST_LENGTH * 2 + 1];
+  snprint_hex (buf, sizeof (buf), key, key_len);
+  return buf;
+}
 
 char *
 block_key (char *key, int *key_len,
            char *parent, int parent_len, int x, int y)
 {
+  void *message;
+  int size;
+
+#ifdef NON_STRING_MESSAGE
   u_int32_t x__y = ((x << 24) | y);
-  void *message = &x__y;
-  int size = sizeof (x__y);
+  message = &x__y;
+  size = sizeof (x__y);
+#else /*NON_STRING_MESSAGE*/
+  char x__y[17];
+  message = x__y;
+  size = sizeof (x__y) - 1;
+  snprintf (x__y, sizeof (x__y), "%016x", ((x << 24) | y));
+#endif /*NON_STRING_MESSAGE*/
+
+#ifdef DEBUG
+  print_hex ("parent = ", parent, parent_len);
+  printf ("x: %d, y: %d, message: %s, size: %d\n", x, y, x__y, size);
+  print_hex ("message = ", x__y, size);
+#endif /*DEBUG*/
 
 #ifdef __APPLE__
   CCHmac (kCCHmacAlgSHA1, parent, parent_len,
           (const void *)message, size, key);
   *key_len = SHA_DIGEST_LENGTH;
-#else /*!__APPLE__*/
+#else /*__APPLE__*/
   HMAC (EVP_sha1(), parent, parent_len,
         (const unsigned char *) message, size,
         key, key_len);
 #endif /*__APPLE__*/
 
+#ifdef DEBUG
+  print_hex ("key = ", key, *key_len);
+#endif /*DEBUG*/
+
   return key;
 }
-
-char *
-key_print (char *key, int key_len)
-{
-  int i;
-  static char string[SHA_DIGEST_LENGTH * 2 + 1];
-
-  for (i = 0; i < key_len; i++)
-    {
-      snprintf (&string[i * 2], 3, "%02x", key[i]);
-    }
-
-  return string;
-}
-
-#define MIN(x,y) ((x) < (y) ? (x) : (y))
 
 void
 usage ()
@@ -84,11 +120,12 @@ main (int argc, char **argv)
 {
   int i;
   int x, y;
-  int offset;
-  int key_len, parent_len;
+  int offset = 4096;
+  char *master = "master";
 
-  char parent[SHA_DIGEST_LENGTH];
+  char parent[SHA_DIGEST_LENGTH * 2 + 1];
   char key[SHA_DIGEST_LENGTH];
+  int parent_len, key_len;
 
   progname = argv[0];
 
@@ -102,23 +139,17 @@ main (int argc, char **argv)
     }
 
   /* Byte offset */
-  offset = 4096;
   if (argc > 1)
-    {
-      offset = atoi (argv[1]);
-    }
+    offset = atoi (argv[1]);
 
   /* Master key */
-  strncpy (key, "master", sizeof (key));
-  key_len = strlen ("master");
   if (argc > 2)
-    {
-      strncpy (key, argv[2], SHA_DIGEST_LENGTH);
-      key_len = MIN (SHA_DIGEST_LENGTH, strlen (argv[2]));
-    }
+    master = argv[2];
+  snprintf (parent, sizeof (parent), "%s", master);
+  parent_len = strlen (master);
 
   printf ("offset = %010d, master = %s(%d).\n",
-          offset, key_print (key, key_len), key_len);
+          offset, print_key (parent, parent_len), parent_len);
 
   /* Out of bound offset. */
   if (offset >= block_size[0])
@@ -127,29 +158,35 @@ main (int argc, char **argv)
       exit (1);
     }
 
-#if 0
+#ifdef DEBUG
   for (i = 0; i < 10; i++)
     printf ("block_size[%d] = %10d\n", i, block_size[i]);
-#endif
+#endif /*DEBUG*/
 
   for (i = 0; i < 10; i++)
     {
       x = i;
       y = offset / block_size[x];
 
-      /* parent = key. */
-      strncpy (parent, key, sizeof (parent));
-      parent_len = key_len;
-
+#ifdef NON_SHA1_ROOT_KEY
       if (x != 0)
         {
+#endif /*NON_SHA1_ROOT_KEY*/
+
           /* calculate K_(x,y). */
           block_key (key, &key_len, parent, parent_len, x, y);
+
+#ifdef NON_SHA1_ROOT_KEY
         }
+#endif /*NON_SHA1_ROOT_KEY*/
 
       printf ("K_(%d,%d) = %s(%d) [%010d-%010d]\n",
-               x, y, key_print (key, key_len), key_len,
+               x, y, print_key (key, key_len), key_len,
                block_size[x] * y, block_size[x] * (y + 1) -1);
+
+      /* parent = str(key) */
+      snprintf (parent, sizeof (parent), "%s", print_key (key, key_len));
+      parent_len = strlen (parent);
     }
 
   return 0;
---
+++ misc/horus.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import hashlib, hmac
+
+
+def block_key (base_key, file_size, byte_offset, start_level=0, end_level=9):
+  '''
+  Return the block key to be used for a given byte offset.  This assumes
+  a maximum chunk size of 1 GB and a minimum chunk size of 4 KB, with a
+  branching factor of 4 between the levels.  This means 9 branches to
+  go from 1 GB down to 4 KB.
+  '''
+  blk_sizes = [1073741824, 268435456, 67108864, 16777216, 4194304,
+               1048576, 262144, 65536, 16384, 4096]
+  if byte_offset >= file_size:
+    raise ValueError
+  if not 0 <= start_level <= len (blk_sizes)-1:
+    raise ValueError, 'invalid start level %d' % (start_level)
+  if not 1 <= end_level <= len (blk_sizes):
+    raise ValueError, 'invalid end level %d' % (end_level)
+  if start_level >= end_level:
+    raise ValueError, 'start level %d must be less than end level %d' % (start_level, end_level)
+  cur_offset = byte_offset
+  if type (base_key) != str:
+    cur_key = '%016x' % base_key
+  else:
+    cur_key = base_key
+  for i in range (start_level, end_level+1):
+    if i > 0:
+      max_blks = blk_sizes[i-1] / blk_sizes[i]
+    else:
+      max_blks = 1000000000
+    #blk_num = (cur_offset / blk_sizes[i]) % max_blks
+    #cur_offset = cur_offset % blk_sizes[i]
+    blk_num = (byte_offset / blk_sizes[i])
+    cur_offset = byte_offset % blk_sizes[i]
+    h = hmac.new (cur_key, '%016x' % ((i << 24) | blk_num), hashlib.sha1)
+    cur_key = h.hexdigest ()
+    print "K" + str(i) + "," + str(blk_num) + " = " + cur_key,
+    print "[" + '%016x' % ((i << 24) | blk_num) + "]"
+
+  return cur_key
+
+
+print "primary=base_key file_size=3M offset=40960"
+print block_key ("base_key", 3000000, 10*4096, 0, 9)
+
