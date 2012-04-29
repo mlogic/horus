ee2679e067aa3966e6e492d0552842100118612a
Nakul Dhotre <nakul@soe.ucsc.edu>
2012-04-29 13:05:58 -0700

    mpi-io support

---
+++ lib/horusio.h
@@ -0,0 +1,28 @@
+#ifndef _HORUSIO_C_
+#define _HORUSIO_C_
+int
+horusio_open (const char *path, int oflag, ...);
+
+int
+horusio_socket (int domain, int type, int protocol);
+
+ssize_t
+horusio_read (int fd, void *buf, size_t size);
+
+ssize_t
+horusio_write (int fd, const void *buf, size_t size);
+
+int
+horusio_close (int fd);
+
+int
+hoursio_unlink (const char *path);
+
+int
+horusio_dup2 (int fd, int fd2);
+
+#endif
+
+
+
+
